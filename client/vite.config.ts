b1e350edd16f11d777301c623498691da889c44c
Jaakko Mäenpää <jaakko.maenpaa@outlook.com>
2024-12-29 17:42:13 +0200

    configure https

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
+import dotenv from 'dotenv'
 
-// https://vite.dev/config/
+const isProduction = process.env.NODE_ENV === 'production'
+
+if (isProduction) {
+  dotenv.config({ path: '.env.production' })
+}
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+    https: isProduction
+      ? {
+          key: fs.readFileSync(
+            path.resolve(__dirname, process.env.VITE_KEY_PATH || '')
+          ),
+          cert: fs.readFileSync(
+            path.resolve(__dirname, process.env.VITE_CERT_PATH || '')
+          ),
+        }
+      : undefined,
   },
 })
