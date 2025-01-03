64ad762a8769eb9393bd1ea9dd576e146c692783
Jaakko Mäenpää <jaakko.maenpaa@outlook.com>
2025-01-03 12:02:40 +0200

    redesign about me and portfolio files

--- client/src/folders/AboutMe.tsx
+++ client/src/folders/AboutMe.tsx
@@ -2,10 +2,17 @@ import { Box, Typography } from '@mui/material'
 
 const AboutMe = () => {
   return (
-    <Box sx={{ padding: '16px' }}>
-      <Typography variant='body1'>
-        I'm a 24-year-old student in Tampere. I also play badminton at the Finnish
-        Championship level.
+    <Box sx={{ padding: '16px', color: 'text.primary' }}>
+      <Typography sx={{ mb: 2 }}>
+        I am a 24-year-old technology student in Tampere University.
+      </Typography>
+      <Typography sx={{ mb: 2 }}>
+        I have always had a strong passion for different problem-solving tasks and
+        programming has been a great way to challenge myself.
+      </Typography>
+      <Typography sx={{ mb: 2 }}>
+        The focus on my personal coding projects has shifted towards web development,
+        since it offers quick, concrete results and a wide range of possibilities.
       </Typography>
     </Box>
   )
--- client/src/folders/Portfolio.tsx
+++ client/src/folders/Portfolio.tsx
@@ -3,7 +3,26 @@ import { Box, Typography } from '@mui/material'
 const Portfolio = () => {
   return (
     <Box sx={{ padding: '16px' }}>
-      <Typography variant='body1'>Welcome to my portfolio!</Typography>
+      <Typography sx={{ mb: 2 }}>Welcome to my portfolio!</Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+        <Typography>
+          This is a web application that I have built to showcase my programming
+          skills. It is built with React, TypeScript and Material UI.
+        </Typography>
+        <Typography>
+          The app mimics an OS with a file system, where you can navigate through
+          different folders and files. This can be done via the file explorer or the
+          command line.
+        </Typography>
+        <Typography>
+          Desktop view can be customized by dragging new items from the explorer or by
+          moving existing ones around.
+        </Typography>
+        <Typography>
+          Customizations are saved to local storage, which can be easily cleared from
+          settings.
+        </Typography>
+      </Box>
     </Box>
   )
 }
