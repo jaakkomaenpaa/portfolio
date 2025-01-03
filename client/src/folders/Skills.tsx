3fa99fc82835004b3408d273362fb22ac92c18b0
Jaakko Mäenpää <jaakko.maenpaa@outlook.com>
2025-01-03 11:40:15 +0200

    redesign skills file

--- client/src/folders/Skills.tsx
+++ client/src/folders/Skills.tsx
@@ -1,18 +1,74 @@
 import { Box, Typography } from '@mui/material'
+import { styled } from '@mui/system'
+
+const SkillRow = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  color: theme.palette.text.secondary,
+  alignItems: 'center',
+  textAlign: 'right',
+}))
+
+const SkillLevel = styled(Box)(
+  ({ theme, level }: { theme?: any; level: number }) => ({
+    height: 20,
+    width: level * 30,
+    backgroundColor: theme.palette.primary.main,
+  })
+)
 
 const Skills = () => {
   return (
-    <Box sx={{ padding: '16px' }}>
-      <Typography sx={{ mb: 1, color: 'text.secondary' }}>Background</Typography>
-      <Typography sx={{ mb: 3 }} variant='body1'>
+    <Box sx={{ padding: '16px', maxHeight: '80vh', overflowY: 'auto' }}>
+      <Typography sx={{ mb: 3 }}>Background</Typography>
+      <Typography sx={{ mb: 2, color: 'text.secondary' }} variant='body1'>
         First started to gain interest in programming in January 2022 through
         university courses.
       </Typography>
-      <Typography sx={{ mb: 1, color: 'text.secondary' }}>Technologies</Typography>
-      <Typography sx={{ mb: 3 }} variant='body1'>
-        Most prominent in React, React Native (Expo), TypeScript (+JS), Express,
-        Python, and SQL. Also experience in C++, C, and Java.
+      <Typography sx={{ mb: 4, color: 'text.secondary' }} variant='body1'>
+        Since then I have been working on various projects of my own, mostly in web
+        development.
       </Typography>
+      <Typography sx={{ mb: 3 }}>Technologies by experience</Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+        <SkillRow>
+          <Typography>TypeScript (+JS)</Typography>
+          <SkillLevel level={5} />
+        </SkillRow>
+        <SkillRow>
+          <Typography>React</Typography>
+          <SkillLevel level={5} />
+        </SkillRow>
+        <SkillRow>
+          <Typography>React Native (Expo)</Typography>
+          <SkillLevel level={4} />
+        </SkillRow>
+        <SkillRow>
+          <Typography>Express.js</Typography>
+          <SkillLevel level={4} />
+        </SkillRow>
+        <SkillRow>
+          <Typography>Python</Typography>
+          <SkillLevel level={4} />
+        </SkillRow>
+        <SkillRow>
+          <Typography>C++</Typography>
+          <SkillLevel level={3} />
+        </SkillRow>
+        <SkillRow>
+          <Typography>C</Typography>
+          <SkillLevel level={1} />
+        </SkillRow>
+        <SkillRow>
+          <Typography>Java</Typography>
+          <SkillLevel level={1} />
+        </SkillRow>
+        <SkillRow>
+          <Typography>SQL (SQLite)</Typography>
+          <SkillLevel level={3} />
+        </SkillRow>
+      </Box>
     </Box>
   )
 }
