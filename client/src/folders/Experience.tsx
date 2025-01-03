c1bd80ba4deacecdb6005f5286c3e6fffbf2c595
Jaakko Mäenpää <jaakko.maenpaa@outlook.com>
2025-01-03 11:01:14 +0200

    redesign experience file

--- client/src/folders/Experience.tsx
+++ client/src/folders/Experience.tsx
@@ -1,22 +1,40 @@
 import { Box, Button, Typography } from '@mui/material'
 import { useWindowStore } from '../stores/WindowStore'
+import { styled } from '@mui/system'
+
+const InfoSection = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 14,
+  backgroundColor: theme.palette.background.paper,
+  padding: 16,
+}))
+
+const TextRow = styled(Box)({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  textAlign: 'right',
+})
 
 const StartupWindow = () => {
   return (
     <Box sx={{ padding: '16px' }}>
-      <Typography sx={{ mb: 1, color: 'text.secondary' }}>General</Typography>
-      <Typography sx={{ mb: 3 }}>
+      <Typography sx={{ mb: 1 }}>General</Typography>
+      <Typography sx={{ mb: 3, color: 'text.secondary' }}>
         Started working in a 4-person startup in April 2024 alongside studies as a
         side business.
       </Typography>
-      <Typography sx={{ mb: 1, color: 'text.secondary' }}>Role</Typography>
-      <Typography sx={{ mb: 3 }}>
+      <Typography sx={{ mb: 1 }}>Product</Typography>
+      <Typography sx={{ mb: 3, color: 'text.secondary' }}>
+        A mobile app designed to ease law interpretation in a certain field of work.
+        Cannot reveal more before launch (in January 2025?).
+      </Typography>
+      <Typography sx={{ mb: 1 }}>Role</Typography>
+      <Typography sx={{ mb: 3, color: 'text.secondary' }}>
         My role is a software developer and I have had the main responsibility in
         developing our mobile app and website.
-      </Typography>
-      <Typography sx={{ mb: 1, color: 'text.secondary' }}>Other</Typography>
-      <Typography sx={{ mb: 3 }}>
-        Cannot disclose the name and product until launch in January 2025.
       </Typography>
     </Box>
   )
@@ -27,17 +45,21 @@ const Experience = () => {
 
   return (
     <Box sx={{ padding: '16px' }}>
-      <Button
-        variant='outlined'
-        sx={{
-          mb: 1,
-          color: 'text.primary',
-          cursor: 'pointer',
-        }}
-        onClick={() => openWindow('Startup', <StartupWindow />)}
-      >
-        Startup
-      </Button>
+      <InfoSection>
+        <TextRow>
+          <Typography>Startup</Typography>
+          <Typography sx={{ color: 'text.secondary' }}>April 2024 -</Typography>
+          <Button
+            variant='outlined'
+            sx={{
+              cursor: 'pointer',
+            }}
+            onClick={() => openWindow('Startup', <StartupWindow />)}
+          >
+            Details
+          </Button>
+        </TextRow>
+      </InfoSection>
     </Box>
   )
 }
